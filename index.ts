622b1e118489e51b6c5bc3fe534dcf6684788d18
Ondřej Chrastina <ondrejch@kontent.ai>
2023-01-17 12:58:51 +0100

    round 2

--- index.ts
+++ index.ts
@@ -6,20 +6,22 @@ let positionY = 0;
 
 fs.readFile('data.txt', 'utf8', (err, data) => {
   if (data) {
-    const txt = data.split("\n");
+    const txt = data.split("\r\n");
     txt.forEach(element => {
       const command = element.split(" ");
-      if (command[0] == "forward"){
-        positionX += command[1];
+      if (command[0] == "forward") {
+        positionX += parseInt(command[1]);
       }
-      else if (command[0] == "down"){
-        positionY += command[1]
+      else if (command[0] == "down") {
+        positionY += parseInt(command[1])
       }
-      else if (command[0] == "up"){
-        positionY -= command[1]
+      else if (command[0] == "up") {
+        positionY -= parseInt(command[1])
       }
     });
-    return;
+
+    console.log(positionX * positionY)
+    return positionX * positionY;
   }
   console.log(data);
 });
