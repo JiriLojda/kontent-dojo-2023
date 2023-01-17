d605cc0a253cef29ab4baae9e0e654362458a748
Ivan Kiral <kiralivan@gmail.com>
2023-01-17 12:47:33 +0100

    round 1

--- index.ts
+++ index.ts
@@ -1,8 +1,24 @@
 import fs from 'fs';
 
+let positionX = 0;
+let positionY = 0;
+
+
 fs.readFile('data.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
+  if (data) {
+    const txt = data.split("\n");
+    txt.forEach(element => {
+      const command = element.split(" ");
+      if (command[0] == "forward"){
+        positionX += command[1];
+      }
+      else if (command[0] == "down"){
+        positionY += command[1]
+      }
+      else if (command[0] == "up"){
+        positionY -= command[1]
+      }
+    });
     return;
   }
   console.log(data);
